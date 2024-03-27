7b7a506a62143af942a2defb2d2493a43e899fd8
Artur Souza <artur.souza955@gmail.com>
2024-03-26 21:57:33 -0300

    implementação de routes

--- frontend/src/app/components/templates/header/header.component.ts
+++ frontend/src/app/components/templates/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule,RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
